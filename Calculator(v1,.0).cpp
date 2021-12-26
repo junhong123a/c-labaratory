572df90bb0c62b586467c63d723164cbffbd8d5c
junhong123a <53944115+junhong123a@users.noreply.github.com>
2021-12-27 01:00:22 +0900

    아무나 깃 사용법좀 알려주셈/goto문 제거 하는법두
    ㅋㅋ

--- Calculator(v1,.0).cpp
+++ Calculator(v1,.0).cpp
@@ -7,12 +7,14 @@ using namespace std;
 
 //start function
 void start() {
-	cout << "계산기 v1.0\nMaker : Junhong(Developer)" << endl;
+	cout << "계산기 v1.1\nMaker : Junhong(Developer)" << endl;
+	cout << "계산기는 계속 개발중임!";
 }
+
 int main() {
 	start();
 
-	int p1, p2, t = 0;
+	int p1, p2, bp = 0;
 	char p3;
 
 	makeresult:
@@ -43,8 +45,23 @@ int main() {
 		goto makeresult;
 		break;
 	}
+
+backpoint:
+
+	cout << "처음으로 돌아가기(1), 종료하기(0):";
+	cin >> bp;
+	switch (bp) {
+	case 1:
+		goto makeresult;
+		break;
+	case 0:
+		return 0;
+		break;
+	default:
+		cout << "잘못 입력하셨습니다. 다시 입력해주세요!" << endl;
+		goto backpoint;
+		break;
+	}
 	
 	return 0;
 }
-
-//~~이거 커밋 해야하는건가 깃헙에 ㄱㄷ 하고 옴~~
