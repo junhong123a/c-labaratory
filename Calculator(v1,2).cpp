8c9126ea07c4dae7c8d89abdd80b8269944eab66
성준홍 <53944115+junhong123a@users.noreply.github.com>
2022-02-02 01:20:08 +0900

    Release 1.0
    엑스퍼트님 감사합니다

--- Calculator(v1,2).cpp
+++ Calculator(v1,2).cpp
@@ -1,52 +1,54 @@
-//including modules
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-//start function
 void start() {
-	cout << "계산기 v1.1\nMaker : Junhong(Developer)" << endl;
+	cout << "계산기 v1.0\nMaker : Junhong(Developer)" << endl;
 	cout << "계산기는 계속 개발중임!" << endl;
+	cout << "해당 계산기 코드는 네이버 지식IN 엑스퍼트의 도움을 받아 제작되었으며, 무단 복제 시 법적 처벌을 받을 수 있습니다." << endl;
 }
 
 int main() {
 	start();
 
-	int i = 0, p1, p2 = 0;
+	int p1, p2 = 0;
+	int result = 0;
 	char p3;
-	
-	makeresult:
-	cout << "첫번째 수 입력:";
-	cin >> p1;
-	cout << "두번째 수 입력:";
-	cin >> p2;
 
-	cout << "사칙연산 부호 입력: ";
-	cin >> p3;
+	while (true)
+	{
+		cout << "\n첫번째 수 입력:";
+		cin >> p1;
+		cout << "두번째 수 입력:";
+		cin >> p2;
 
-	switch (p3) {
-	case '+':
-		cout << "계산 결과:" << p1 + p2 << endl;
-		i++;
-		break;
-	case '-':
-		cout << "계산 결과:" << p1 - p2 << endl;
-		i++;
-		break;
-	case '*':
-		cout << "계산 결과:" << p1 * p2 << endl;
-		i++;
-		break;
-	case '/':
-		cout << "계산 결과:" << p1 / p2 << endl;
-		i++;
-		break;
-	default:
-		cout << "잘못 입력하셨습니다. 다시 입력해주세요!" << endl;
-		goto makeresult:
-		break;
+		if (p1 == 0 && p2 == 0)
+			break;
 
+		cout << "사칙연산 부호 입력: ";
+		cin >> p3;
+
+
+		switch (p3) {
+		case '+':
+			result = p1 + p2;
+			break;
+		case '-':
+			result = p1 - p2;
+			break;
+		case '*':
+			result = p1 * p2;
+			break;
+		case '/':
+			result = p1 / p2;
+			break;
+		default:
+			cout << "잘못 입력하셨습니다. 다시 입력해주세요!" << endl;
+			break;
+		}
+
+		if (p3 == '+' || p3 == '-' || p3 == '*' || p3 == '/')
+			cout << "계산 결과:" << result << endl;
+	}
 }
-
-//~~이거 커밋 해야하는건가 깃헙에 ㄱㄷ 하고 옴~~//including modules
